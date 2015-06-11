73baf009656d598dd57c61af2311ec0a1597a15b
Jared Quinn <jared@jaredquinn.info>
2015-06-11 13:43:24 +1000

    Use HTTP, Fix localhost, logging & typo

--- hello_app/hello.js
+++ hello_app/hello.js
@@ -1,11 +1,11 @@
 var os = require('os');
-var http = require('https');
+var http = require('http');
 var port = 8902;
-var bindip = "127.0.0.3";
+var bindip = "127.0.0.1";
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end( 'Hello World from ' + os.hostname() + '!\n' ),
+  response.end( 'Hello World from ' + os.hostname() + '!\n' );
 }).listen(port);
 
-console.logging('Server running at https://' + bindip + ':' + port + '/');
+console.log('Server running at https://' + bindip + ':' + port + '/');
